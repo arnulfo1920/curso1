0d25f9d01eacd259da6eb9b4a4d9c5be2036cbfe
arnulfo perez alvarez <36308755+arnulfo1920@users.noreply.github.com>
2018-02-09 13:14:44 -0600

    crud de clientes
    prueba 1

---
+++ header.php
@@ -0,0 +1,40 @@
+<html lang="es">
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/bootstrap-theme.css" rel="stylesheet">
+		<script src="js/jquery-3.1.1.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>	
+	</head>
+	<body>
+<nav class="navbar navbar-inverse">
+	<div class="container-fluid">
+		<div class="navbar-header">			
+			<a class="navbar-brand" href="http://localhost/plantilla-base/">
+			<span class="glyphicon glyphicon-cloud" aria-hidden="true" ></span>
+			sistema de registros
+			</a>
+		</div>
+			<ul class="nav navbar-nav">
+				<li class="active">
+					<a href="?menu=clientes">clientes</a>
+				</li>
+				<li>
+					<a href="?menu=direcciones">direcciones</a>
+				</li>
+				<li>
+					<a href="?menu=registros">registros</a>
+				</li>
+			</ul>
+	</div>
+</nav>
+<body>
+	</!DOCTYPE html>
+	<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+	
+	</body>
+	</html>>
